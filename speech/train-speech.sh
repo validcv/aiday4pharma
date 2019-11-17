96c61f78a9d8f19d3a2fffd5549ff9838a961aba
srija-ieee <42797168+srija-ieee@users.noreply.github.com>
2019-11-17 21:24:08 +0530

    Update train-speech.sh

--- speech/train-speech.sh
+++ speech/train-speech.sh
@@ -1,3 +1,9 @@
+# Fix dependencies for DeepQA
+pip3 install tqdm
+pip3 install nltk
+echo "import nltk; nltk.download('punkt'); exit()" | python3
+
+# Copy the dataset and train
 cp $1.txt DeepQA/data/lightweight/
-cd DeepQA/
+cd DeepQA
 ./main.py --corpus lightweight --datasetTag $1
